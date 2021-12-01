9106d2ef1fe81086f97c492d94d3291ebf0bc07a
refael test <refael058325@gmail.com>
2021-12-01 11:59:18 +0000

    add function webhook

--- src/web.py
+++ src/web.py
@@ -1,6 +1,15 @@
 import requests
 import json
 webhook_url = 'http://127.0.0.1:5000//webhook'
-data = { 'name': 'This is an example for webhook',
-            'refael': 'data' }
+data = { 'namespace': 'fre',
+        'host': 'test',
+        'repo': 'https://github.com/refaelb/image_project.git',
+        'tag': 'v0.0.1',
+        'reg': 'refael058325',
+        'branch': 'dev',
+        'ingress': 'ew',
+        'Ruser': 'refael058325',
+        'Rpass': 'Rootroot316444058',
+        'Duser': 'refaelb',
+        'Dpass': 'Refael'}
 requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
